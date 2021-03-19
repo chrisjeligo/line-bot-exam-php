d1a92291c4ac9c7dc3893d847b228203b433b02e
chrisjeligo <chrisjeligo@gmail.com>
2021-03-19 16:48:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '1eKrmMLLa94TW9QMwMMrqO7nT6o3KMMGeuWCQqEAkZIoplNssWXgud2gEOqFGxgF+v6OnPLM7JLzhZt6rU9DliNrJpWHJZXA+8orI0/jKYXbk8wJYjzXpsD63rVZFgquq4vUkVqg2gc5oi5Ik8o6wwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
